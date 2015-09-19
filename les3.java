2e7efa861b9e25539edb780f9e117d301545eb77
ZombSmile <ksieniia.pavlova.97@mail.ru>
2015-09-19 23:06:40 +0300

    probny

---
+++ les3.java
@@ -0,0 +1,15 @@
+public class les3 {
+	public static void main(String[] args) {
+    double pi = 3.14;
+    double r = 10;
+    double V;
+   
+    V = r * r * r * pi * 4/3;
+
+         System.out.print("Radius = ");
+         System.out.println(r);
+         
+         System.out.print("Volume = ");
+         System.out.println(V);
+    }
+}
