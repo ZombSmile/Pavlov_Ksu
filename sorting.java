5c8f8b39721e70b98f7ce6179614b36d6def5cd4
ZombSmile <ksieniia.pavlova.97@mail.ru>
2015-10-06 23:31:07 +0300

    HomeWork

--- sorting.java
+++ sorting.java
@@ -1,27 +1,37 @@
 import java.util.Arrays;
 
 public class sorting {
-public static void main(String[] args) {
 
-	int n = Integer.valueOf(args [0]), j = n;
-	int[] a = new int[n];
-	for (int i = 0; i < n; i++) {
-      a[i] = (int) Math.round(Math.random() * 100);
+  public static void main(String[] args) {
+    double startTime, timeDifferece;
+    int size = 10;
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++) {
+      array[i] = (int) Math.round(Math.random() * 100 );
     }
-	
-	System.out.println(Arrays.toString(a));
-		while ( j > 0 ) {
-			j--;
-			for( int i = 0; i < n-j; i++){
-				if(a[i]>a[i+j]){
-					int p = a[i];
-					a[i] = a[j+i];
-					a[j+i] = p;
-				}
-			}
-		}
-		System.out.print(Arrays.toString(a));
-
-
-} 
+    hairbrush( array , size );
+    for (int i = 0; i < size ; i++) {
+      System.out.println(array[i]);
+    }
+  }
+  public static void hairbrush(int[] array, int size) {
+    int size1 = size;
+    int t;
+    boolean swap = true;
+    while (size1 > 1 || swap) {
+        if (size1 > 1) 
+            size1 = (int) (size1 / 1.24);
+        int i = 0;
+        swap = false;
+        while (i + size1 < size) {
+            if (array[i] > array[i + size1]) {
+                t = array[i];
+                array[i] = array[i + size1];
+                array[i + size1] = t;
+                swap = true;
+            }
+            i++;
+        }
+    }
+  }
 }
